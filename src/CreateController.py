5064e35bc862fa1a728cee16a4d43808b871163a
profLi <jili1@uiwtx.edu>
2024-04-04 14:52:14 -0500

    set window size

--- src/CreateController.py
+++ src/CreateController.py
@@ -7,7 +7,7 @@ class CreateControllerWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Create IKFK Limb")
-
+        self.setGeometry(100,100,300,300)
 
 controllerWidget = CreateControllerWidget()
 controllerWidget.show()
