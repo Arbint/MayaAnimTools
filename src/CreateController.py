ec4156b66f315abe32bbf6f3a3197ff62749617b
profLi <jili1@uiwtx.edu>
2024-04-04 14:43:29 -0500

    added window

--- src/CreateController.py
+++ src/CreateController.py
@@ -1,3 +1,14 @@
 #press alt + shift + m 
 import maya.cmds as mc
 
+from PySide2.QtWidgets import QWidget
+
+class CreateControllerWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Create IKFK Limb")
+
+
+controllerWidget = CreateControllerWidget()
+controllerWidget.show()
+
